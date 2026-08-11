b4926e569de0e432c765d3b30f093ff988f2118a
Aayesha-1428 <mdaayeshabegum@gmail.com>
2026-08-11 16:57:58 +0530

    Delete assignment-1/aayesha1.ts

--- assignment-1/aayesha1.ts
+++
@@ -1,9 +0,0 @@
-function greet(name:string):string{
-    return `namaste,${name}!`;
-}
-function getLocation(city:string="Hyderabad"):string{
-    return `Location:${city};
-}
-
-function sendAlert(phoneNumber:number,message?:string):void{
-}
